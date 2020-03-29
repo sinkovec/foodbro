5a86858c7130fb7ba249c7f99771248122eb4702
Brian Sinkovec <brian.sinkovec@rwth-aachen.de>
2020-03-29 12:10:41 +0200

    drag and drop list items in preparations edit fragment

--- app/src/main/java/de/foodbro/app/ui/edit/PreparationsAdapter.kt
+++ app/src/main/java/de/foodbro/app/ui/edit/PreparationsAdapter.kt
@@ -4,6 +4,7 @@ import de.foodbro.app.R
 import de.foodbro.app.databinding.ListItemEditPreparationBinding
 import de.foodbro.app.model.PreparationStep
 import de.foodbro.app.ui.adapter.AbstractPreparationAdapter
+import java.util.*
 
 class PreparationsAdapter : AbstractPreparationAdapter<ListItemEditPreparationBinding>() {
 
@@ -12,6 +13,22 @@ class PreparationsAdapter : AbstractPreparationAdapter<ListItemEditPreparationBi
     override fun getViewHolder(binding: ListItemEditPreparationBinding):
             AbstractPreparationAdapter.ViewHolder<ListItemEditPreparationBinding> =
         ViewHolder(binding)
+
+    fun swap(fromPosition: Int, toPosition: Int) {
+        val list = currentList.toMutableList()
+        Collections.swap(list, fromPosition, toPosition)
+
+        list[fromPosition].pos = toPosition + 1
+        list[toPosition].pos = fromPosition + 1
+
+        if (fromPosition < toPosition) {
+            notifyItemMoved(fromPosition, toPosition)
+        } else {
+            notifyItemMoved(toPosition, fromPosition)
+        }
+        notifyItemChanged(fromPosition)
+        notifyItemChanged(toPosition)
+    }
 
     inner class ViewHolder(binding: ListItemEditPreparationBinding) :
         AbstractPreparationAdapter.ViewHolder<ListItemEditPreparationBinding>(binding) {
--- app/src/main/java/de/foodbro/app/ui/edit/RecipeEditPreparationFragment.kt
+++ app/src/main/java/de/foodbro/app/ui/edit/RecipeEditPreparationFragment.kt
@@ -1,15 +1,15 @@
 package de.foodbro.app.ui.edit
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
-import dagger.android.support.DaggerFragment
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.RecyclerView
 import de.foodbro.app.databinding.FragmentRecipeEditPreparationBinding
-import javax.inject.Inject
+import java.util.*
 
 class RecipeEditPreparationFragment : Fragment() {
 
@@ -21,15 +21,33 @@ class RecipeEditPreparationFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        viewDataBinding = FragmentRecipeEditPreparationBinding.inflate(inflater, container, false).apply {
-            viewModel = this@RecipeEditPreparationFragment.viewModel
-            lifecycleOwner = this@RecipeEditPreparationFragment.viewLifecycleOwner
-        }
+        viewDataBinding =
+            FragmentRecipeEditPreparationBinding.inflate(inflater, container, false).apply {
+                viewModel = this@RecipeEditPreparationFragment.viewModel
+                lifecycleOwner = this@RecipeEditPreparationFragment.viewLifecycleOwner
+            }
         return viewDataBinding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         val listAdapter = PreparationsAdapter()
+
+        val touchHelper = ItemTouchHelper(object :
+            ItemTouchHelper.SimpleCallback(ItemTouchHelper.UP or ItemTouchHelper.DOWN, 0) {
+            override fun onMove(
+                recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                listAdapter.swap(viewHolder.adapterPosition, target.adapterPosition)
+                return true
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+            }
+
+        })
+        touchHelper.attachToRecyclerView(viewDataBinding.preparationsList)
+
         viewDataBinding.preparationsList.adapter = listAdapter
         viewModel.preparationSteps.observe(viewLifecycleOwner, Observer {
             listAdapter.submitList(it)
